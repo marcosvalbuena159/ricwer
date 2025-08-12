431030d575197576cc6062a04f41b62af6c64895
marcosvalbuena159 <marcos.valbuena560@gmail.com>
2025-08-11 21:30:48 -0500

    Delete App directory

--- App/Controlador/Eliminar/ctrl_EliminarUsuario.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once('../../modelo/Cls_Usuario.php');
-    $objUsuario     =   new clsUsuario();
-    $idUsuario      =   $_GET['ID'];
-     
-    $objUsuario->setidUsuario($idUsuario);
-    $objUsuario->EliminarUsuario();
-?>
