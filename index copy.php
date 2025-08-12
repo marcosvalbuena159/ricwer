c4d7ac63893736f6c94cc1ec3a1fc26c4c77aafe
marcosvalbuena159 <marcos.valbuena560@gmail.com>
2025-08-11 21:33:55 -0500

    commit -4

---
+++ index copy.php
@@ -0,0 +1,402 @@
+<!doctype html>
+<html lang="es" class="html">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Bienvenido(a) al sitio oficial de ricwer. Aqui encontraras distintos diseños de zapatos para toda la familia de la mejor calidad y al mejor precio. ¡Conoce más!">
+    <meta name="robots" content="noindex,follow">
+    <meta property="og:title" content="ricwer Tienda Oficial | Calzado">
+    <meta property="og:description" content="Bienvenido(a) al sitio oficial de ricwer. Aqui encontraras distintos diseños de zapatos para toda la familia de la mejor calidad y al mejor precio. ¡Conoce más!">
+    <meta property="og:site_name" content="ricwer">
+    <meta property="og:url" content="https://www.ricwer.com">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="public/css/galeria_productos.css">
+    <link rel="stylesheet" href="public/css/main.css">
+    <link rel="stylesheet" href="public/css/carga.css">
+    <link rel="stylesheet" href="index.php">
+    <link rel="icon" href="public/img/001.jpegicono.ico">
+    <title>Ricwer</title>
+  </head>
+  <body>
+    <div id="contenedor_carga">
+		<div id="carga"></div>
+	</div>
+                        <!-- ENCABEZADO -->
+    <header class="container-fluid bg-primary d-flex justify-content-center">
+        <p class="text-light mb-0 p-2 fs-6">Contactanos +57 300-311-8379</p>
+    </header>
+
+    <nav  class="navbar navbar-expand-lg navbar-light p-3"  id="menu" class="navbar bg-body-tertiary">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            <img src="public/img/logo.png" alt="logo"   width="100%" height="100%">
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <nav class="menu">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="nav justify-content-center">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Calzado
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="app/vista/calzado_hombre.php">Hombre</a></li>
+            <li><a class="dropdown-item" href="app/vista/calzado_mujer.php">Mujer</a></li>
+            <li><a class="dropdown-item" href="app/vista/calzado_niños.php">Niños</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="app/vista/empresa.php">Empresa</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="app/vista/login.php">Inicio Sesion</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="app/vista/carrito.php">Mi Carrito</a>
+        </li>
+      </ul>
+      <!--<form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>-->
+    </div>
+    </nav>
+          </div>
+        </div>
+</nav>
+
+
+        <!--========================================================== -->
+                        <!-- INTRODUCCION DE SERVICIOS-->
+    <!--========================================================== -->
+
+    
+
+       <div id="carousel" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active" data-bs-interval="3000">
+            <img src="public/img/carrusel1.png" width="100%" alt="public/img/011.jpeg">
+          </div>
+         
+ 
+          <div class="carousel-item" data-bs-interval="3000">
+            <img src="public/img/carrusel2.png" width="100%" alt="public/img/012.jpeg">
+          </div>
+ 
+ 
+          <div class="carousel-item" data-bs-interval="3000">
+            <img src="public/img/carrusel3.png" width="100%" alt="public/img/013.jpeg">
+          </div>
+ 
+          <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carousel"  data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carousel"  data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>  
+      <br>
+      <br>
+      <br>
+      <section class="d-flex flex-column justify-content-center align-items-center pt-5  text-center w-50 m-auto" id="intro">
+        <p class="p-3  fs-4">
+           <b>ENCUENTRE LOS MEJORE ZAPATOS PARA TODA LA FAMILIA CON LA MEJOR CALIDAD Y A UN BUEN PRECIO</b>     
+        </p>   
+       </section>
+       
+<br>
+      <section class="d-flex flex-column justify-content-center align-items-center pt-5  text-center w-50 m-auto" id="intro">
+        <p class="p-3  fs-4">
+           <b>Productos mas populares.</b>     
+        </p>   
+       </section>
+       <section class="contenido">
+        <div class="mostrador" id="mostrador">
+            <div class="fila">
+                <div class="item" onclick="cargar(this)">
+                    <div class="contenedor-foto">
+                        <img src="public/img/001.jpeg" alt="">
+                    </div>
+                    <p class="descripcion">NIKE AIR 97</p>
+                    <span class="precio">$ 78.000</span>
+                </div>
+                <div class="item" onclick="cargar(this)">
+                    <div class="contenedor-foto">
+                        <img src="public/img/002.jpeg" alt="">
+                    </div>
+                    <p class="descripcion" id>NIKE RUNNING TERRA </p>
+                    <span class="precio">$ 82.000</span>
+                </div>
+                <div class="item" onclick="cargar(this)">
+                    <div class="contenedor-foto">
+                        <img src="public/img/103.jpeg" alt="">
+                    </div>
+                    <p class="descripcion">NIKE WINFLO 8
+                    </p>
+                    <span class="precio">$ 67.000</span>
+                </div>
+                <div class="item" onclick="cargar(this)">
+                    <div class="contenedor-foto">
+                        <img src="public/img/104.jpeg" alt="">
+                    </div>
+                    <p class="descripcion">NIKE LOW-TOP</p>
+                    <span class="precio">$ 80.000</span>
+                </div>
+            </div>
+
+            <div class="fila">
+                <div class="item" onclick="cargar(this)">
+                    <div class="contenedor-foto">
+                        <img src="public/img/003.jpeg" alt="">
+                    </div>
+                    <p class="descripcion">NIKE AIR 97</p>
+                    <span class="precio">$ 78.000</span>
+                </div>
+                <div class="item" onclick="cargar(this)">
+                    <div class="contenedor-foto">
+                        <img src="public/img/004.jpeg" alt="">
+                    </div>
+                    <p class="descripcion" id>NIKE RUNNING TERRA </p>
+                    <span class="precio">$ 82.000</span>
+                </div>
+                <div class="item" onclick="cargar(this)">
+                    <div class="contenedor-foto">
+                        <img src="public/img/101.jpeg" alt="">
+                    </div>
+                    <p class="descripcion">NIKE WINFLO 8
+                    </p>
+                    <span class="precio">$ 67.000</span>
+                </div>
+                <div class="item" onclick="cargar(this)">
+                    <div class="contenedor-foto">
+                        <img src="public/img/102.jpeg" alt="">
+                    </div>
+                    <p class="descripcion">NIKE LOW-TOP</p>
+                    <span class="precio">$ 80.000</span>
+                </div>
+            </div>
+            
+        </div>
+        <!-- CONTENEDOR DEL ITEM SELECCIONADO -->
+        <div class="seleccion" id="seleccion">
+            <div class="cerrar" onclick="cerrar()">
+                &#x2715
+            </div>
+            <div class="info">
+                <img src="public/img/001.jpeg" alt="" id="img">
+                <h2 id="modelo">NIKE MODEL 1</h2>
+                <p id="descripcion">Descripción Modelo 1</p>
+
+                <span class="precio" id="precio">$ 75.000</span>
+
+                <div class="fila">
+                    <div class="size">
+                        <label for="">Talla</label>
+                        <select name="" id="">
+                            <option value="">36</option>
+                            <option value="">37</option>
+                            <option value="">38</option>
+                            <option value="">39</option>
+                            <option value="">40</option>
+                            <option value="">41</option>
+                            <option value="">42</option>
+                            <option value="">43</option>
+                        </select>
+                    </div>
+                    <button>AGREGAR AL CARRITO</button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="d-flex flex-column justify-content-center align-items-center pt-5  text-center w-50 m-auto" id="intro">
+        <p class="p-3  fs-4">
+           <b>Productos mas populares.</b>     
+        </p>   
+       </section>
+       <section class="contenido">
+        <div class="mostrador" id="mostrador">
+            <div class="fila">
+                <div class="item" onclick="cargar(this)">
+                    <div class="contenedor-foto">
+                        <img src="public/img/001.jpeg" alt="">
+                    </div>
+                    <p class="descripcion">NIKE AIR 97</p>
+                    <span class="precio">$ 78.000</span>
+                </div>
+                <div class="item" onclick="cargar(this)">
+                    <div class="contenedor-foto">
+                        <img src="public/img/002.jpeg" alt="">
+                    </div>
+                    <p class="descripcion" id>NIKE RUNNING TERRA </p>
+                    <span class="precio">$ 82.000</span>
+                </div>
+                <div class="item" onclick="cargar(this)">
+                    <div class="contenedor-foto">
+                        <img src="public/img/103.jpeg" alt="">
+                    </div>
+                    <p class="descripcion">NIKE WINFLO 8
+                    </p>
+                    <span class="precio">$ 67.000</span>
+                </div>
+                <div class="item" onclick="cargar(this)">
+                    <div class="contenedor-foto">
+                        <img src="public/img/104.jpeg" alt="">
+                    </div>
+                    <p class="descripcion">NIKE LOW-TOP</p>
+                    <span class="precio">$ 80.000</span>
+                </div>
+            </div>
+
+            <div class="fila">
+                <div class="item" onclick="cargar(this)">
+                    <div class="contenedor-foto">
+                        <img src="public/img/003.jpeg" alt="">
+                    </div>
+                    <p class="descripcion">NIKE AIR 97</p>
+                    <span class="precio">$ 78.000</span>
+                </div>
+                <div class="item" onclick="cargar(this)">
+                    <div class="contenedor-foto">
+                        <img src="public/img/004.jpeg" alt="">
+                    </div>
+                    <p class="descripcion" id>NIKE RUNNING TERRA </p>
+                    <span class="precio">$ 82.000</span>
+                </div>
+                <div class="item" onclick="cargar(this)">
+                    <div class="contenedor-foto">
+                        <img src="public/img/101.jpeg" alt="">
+                    </div>
+                    <p class="descripcion">NIKE WINFLO 8
+                    </p>
+                    <span class="precio">$ 67.000</span>
+                </div>
+                <div class="item" onclick="cargar(this)">
+                    <div class="contenedor-foto">
+                        <img src="public/img/102.jpeg" alt="">
+                    </div>
+                    <p class="descripcion">NIKE LOW-TOP</p>
+                    <span class="precio">$ 80.000</span>
+                </div>
+            </div>
+            
+        </div>
+        <!-- CONTENEDOR DEL ITEM SELECCIONADO -->
+        <div class="seleccion" id="seleccion">
+            <div class="cerrar" onclick="cerrar()">
+                &#x2715
+            </div>
+            <div class="info">
+                <img src="public/img/001.jpeg" alt="" id="img">
+                <h2 id="modelo">NIKE MODEL 1</h2>
+                <p id="descripcion">Descripción Modelo 1</p>
+
+                <span class="precio" id="precio">$ 75.000</span>
+
+                <div class="fila">
+                    <div class="size">
+                        <label for="">Talla</label>
+                        <select name="" id="">
+                            <option value="">36</option>
+                            <option value="">37</option>
+                            <option value="">38</option>
+                            <option value="">39</option>
+                            <option value="">40</option>
+                            <option value="">41</option>
+                            <option value="">42</option>
+                            <option value="">43</option>
+                        </select>
+                    </div>
+                    <button class="boton-item">AGREGAR AL CARRITO</button>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <br>
+    <br>
+<!--========================================================== -->
+                        <!--FOOTER-->
+<!--========================================================== -->
+
+
+<footer class="footer">
+    <div class="container">
+        <div class="footer-row">
+        <div class="footer-links">
+                <h4>Compañia</h4>
+                <ul>
+                    <li><a href="app/vista/empresa.php">Nosotros</a></li>
+                    <li><a href="app/vista/empresa.php">Nuestros Servicios</a></li>
+                    <li><a href="app/vista/empresa.php">Politica de Privacidad</a></li>
+                    <li><a href="app/vista/empresa.php">Informacion</a></li>
+                </ul>
+            </div>
+            <div class="footer-links">
+                <h4>Ayuda</h4>
+                <ul>
+                    <li><a href="app/vista/empresa.php">Sugerencias</a></li>
+                    <li><a href="app/vista/empresa.php">Quejas</a></li>
+                    <li><a href="app/vista/empresa.php">Reclamos</a></li>
+                    <li><a href="app/vista/empresa.php">Soporte</a></li>
+                </ul>
+            </div>
+            <div class="footer-links">
+                <h4>Tienda</h4>
+                <ul>
+                    <li><a href="app/vista/calzado_hombre.php">Calzado Hombre</a></li>
+                    <li><a href="app/vista/calzado_mujer.php">Calzado Mujer</a></li>
+                    <li><a href="app/vista/calzado_niños.php">Calzado Niños</a></li>
+                    <li><a href="app/vista/menu.php">Perfil</a></li>
+                </ul>
+            </div>
+            <div class="footer-links" id="iconos">
+                <h4>Siguenos</h4>
+                <ul>
+                    <li><a href="www.facebook.com/ricwer/"><i class="bi bi-facebook"></i></a></li>
+                    <li><a href="www.instagram.com/ricwer_oficial/"><i class="bi bi-instagram"></i></a></li>
+                    <li><a href="www.x.com/ricwer_oficial/"><i class="bi bi-twitter"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script> 
+    <script src="https://unpkg.com/typewriter-effect@latest/dist/core.js"></script>
+    <script src="public/js/main.js"></script>
+    <script>
+      window.onload=function(){
+        var contenedor = document.getElementById('contenedor_carga');
+      contenedor.style.visibility = 'hidden';
+      contenedor.style.opacity = '0';
+    }
+    </script>
+  </body>
+  
+</html>
+
+    
+
+
